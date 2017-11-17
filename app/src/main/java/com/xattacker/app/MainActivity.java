e1859765c07e18f6773bc50acd8cd2a209cfb7f2
xattacker <xattacker.tao@gmail.com>
2017-11-17 23:27:09 +0800

    upload project first time
    upload project first time

---
+++ app/src/main/java/com/xattacker/app/MainActivity.java
@@ -0,0 +1,89 @@
+package com.xattacker.app;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.android.CaptureActivity;
+import com.google.zxing.client.android.Intents;
+
+public class MainActivity extends AppCompatActivity
+{
+    private final static int REQUEST_CODE = 555;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        for (int r : grantResults)
+        {
+            if (r == PackageManager.PERMISSION_DENIED)
+            {
+                return;
+            }
+        }
+
+        callScan();
+    }
+
+    @Override
+    protected void onActivityResult(int aRequestCode, int aResultCode, Intent aIntent)
+    {
+        super.onActivityResult(aRequestCode, aResultCode, aIntent);
+
+        if (aResultCode != RESULT_OK)
+        {
+            return;
+        }
+
+
+        if (aRequestCode == REQUEST_CODE)
+        {
+            // get 2d code scan result
+            String format = aIntent.getStringExtra(Intents.Scan.RESULT_FORMAT);
+            if (format != null && format.equals(BarcodeFormat.QR_CODE.toString()))
+            {
+                String content = aIntent.getStringExtra(Intents.Scan.RESULT);
+                android.util.Log.i(MainActivity.class.getName(), content);
+                Toast.makeText(this, content, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    // on button click
+    public void onClick(View aView)
+    {
+        callScan();
+    }
+
+    private void callScan()
+    {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions
+            (
+            this,
+            new String[]{Manifest.permission.CAMERA},
+            1
+            );
+        }
+        else
+        {
+            // TWO_D_CODE formats includes "QR_CODE", "DATA_MATRIX", "PDF_417", "AZTEC"
+            CaptureActivity.showActivity(this, Intents.Scan.TWO_D_CODE_FORMATS, REQUEST_CODE);
+        }
+    }
+}
