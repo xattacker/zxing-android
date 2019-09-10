17f260f1d47e53eec1ac8c3a231cdbdaf1500655
xattacker <xattacker.tao@gmail.com>
2019-09-10 09:27:36 +0800

    migrated package to AndroidX

--- app/src/main/java/com/xattacker/app/MainActivity.java
+++ app/src/main/java/com/xattacker/app/MainActivity.java
@@ -4,15 +4,16 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.client.android.CaptureActivity;
 import com.google.zxing.client.android.Intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity
 {
